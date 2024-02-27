4dde6fbd225074cf046a5905f1a46bd72c12185c
ALİ RIZA ÇELİK <aliriza.celik@dijitalkurye.com.tr>
2024-02-27 11:08:32 +0300

    Initial Commit

---
+++ com.cranked/com_crankedApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_crankedApp.swift
+//  com.cranked
+//
+//  Created by Ali Rıza Çelik on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct com_crankedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
